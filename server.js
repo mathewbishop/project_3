7e7ae2c2c8c414604ca085999601aa030b2d134f
Mathew <39043451+mathewbishop@users.noreply.github.com>
2019-01-18 21:59:31 -0600

    Add DB seed data. Removed some dirs and files. Rename some files.

--- server.js
+++ server.js
@@ -4,8 +4,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const mongoose = require("mongoose");
-const routes = require("./controller/routes");
+const baseRoutes = require("./controller/index");
 //============================================================
 // PORT 
 //============================================================
@@ -15,20 +14,7 @@ const PORT = process.env.PORT || 3001;
 //============================================================
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes)
-//============================================================
-// MongoDB Connection
-//============================================================
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/petDB";
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-//============================================================
-// Get Info On DB Connection
-//============================================================
-let db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log("Connected to database.")
-});
+app.use(baseRoutes)
 //============================================================
 // Serve up static assets (Heroku Deployment Essential)
 //============================================================
