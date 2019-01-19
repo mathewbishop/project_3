a30aaef8f1dc1c27f3aab3bc7c91fdabc7c8043b
Mathew <39043451+mathewbishop@users.noreply.github.com>
2019-01-19 10:20:18 -0600

    Working on DB connection, delete controller dir, add routes dir, add user as unique field in petProfile schema.

--- model/dbConnection.js
+++ model/dbConnection.js
@@ -15,3 +15,8 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log("Connected to database.")
 });
+
+module.exports = {
+  MONGODB_URI,
+  db
+}
--- routes/api/index.js
+++ routes/api/index.js
@@ -4,6 +4,3 @@
 // const express = require("express");
 // const router = express.Router();
 // const petProfile = require("../model/petProfileSchema");
-//============================================================
-// API Routes
-//============================================================
