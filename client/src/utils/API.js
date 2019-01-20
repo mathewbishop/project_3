706f6bee7a67dbf765753b598187a34d4d9ffd34
Callie Hart <callie_miller@hotmail.com>
2019-01-19 18:01:51 -0600

    conflict fix

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
- import axios from "axios";
-
- export default {
-
-     getPets: function() {
-         return axios.get("/api/pets");
-     }
-=======
 import axios from "axios";
 
 export default {
@@ -14,7 +5,6 @@ export default {
     getPets: function() {
         return axios.get("/api/pets");
     },
->>>>>>> d1939827e5de7375784187bec6e8d49220c0d7e5
 
     getPets: function(id) {
         return axios.get("/api/pets" + id);
@@ -24,14 +14,7 @@ export default {
         return axios.delete("/api/pets" + id);
     },
 
-<<<<<<< HEAD
-//     savePets: function(petData) {
-//         return axios.post("/api/pets", petData);
-//     }
- };
-=======
     savePets: function(petData) {
         return axios.post("/api/pets", petData);
     }
 };
->>>>>>> d1939827e5de7375784187bec6e8d49220c0d7e5
