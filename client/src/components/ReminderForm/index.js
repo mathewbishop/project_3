02086b33a330dd2355671530bc12413f485eafde
Jordan Beaman <Jordanbeaman94@gmail.com>
2019-01-26 12:50:49 -0600

    Changes to Reminders

--- client/src/components/ReminderForm/index.js
+++ client/src/components/ReminderForm/index.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, {Component} from "react";
 import "./style.css";
+import { Form, FormGroup, Label, Input, FormText, Button } from "reactstrap";
 
-/*function ReminderForm() {
-    return(
-        
-    );
+class ReminderForm extends Component{
+    
+    state = {
+        time: 0,
+        period: "",
+        taskName: "",
+        taskNotes: ""
+    };
+
+    handleChange = input => e => {
+        this.setState({ [input]: e.target.value});
+    };
+
+    render() {
+
+        return(
+            <div>
+
+            <FormGroup>
+            <Label for= "taskName">Reminder</Label>
+            <Input name="taskName"></Input>
+            </FormGroup>
+
+            <FormGroup>
+            <Label for= "taskNotes">Notes</Label>
+            <Input name="taskNotes"></Input>
+            </FormGroup>
+            <Button>Submit</Button>
+
+            </div>
+        )
+  
+    }
 }
 
-export default ReminderForm;*/
+export default ReminderForm;
