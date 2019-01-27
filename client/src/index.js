09c77fdb60eceab9c2ebf6a5d2ec3520d3673256
Mathew <39043451+mathewbishop@users.noreply.github.com>
2019-01-26 18:34:29 -0600

    Remove references from index.

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
-import 'font-awesome/css/font-awesome.css';
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
