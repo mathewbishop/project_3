f2a25737d4dabef50b539596b627518ab48178dd
Mathew <39043451+mathewbishop@users.noreply.github.com>
2019-01-28 17:41:08 -0600

    Remove contacts and reminders controller methods. Restarting.

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -8,13 +8,6 @@ export default {
 
     savePets: function(petData) {
         return axios.post("/api/pets", petData);
-    },
-
-    getReminders: function() {
-        return axios.get("/api/contacts");
-    },
-
-    getContacts: function() {
-        return axios.get("/api/reminders");
     }
+
 };
--- controller/petController.js
+++ controller/petController.js
@@ -17,19 +17,5 @@ module.exports = {
             .create(req.body)
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err))
-    },
-    findContacts: function(req, res) {
-        db.PetProfile
-            .find({})
-            .populate("contacts")
-            .then(dbContacts => res.json(dbContacts))
-            .catch(err => res.status(422).json(err))
-    },
-    findReminders: function(req, res) {
-        db.PetProfile
-            .find({})
-            .populate("reminders")
-            .then(dbReminders => res.json(dbReminders))
-            .catch(err => res.status(422).json(err))
     }
 }
