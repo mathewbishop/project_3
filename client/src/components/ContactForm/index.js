897c487e3a5f5a9e099713f2106a1c313d1824dd
Callie Hart <callie_miller@hotmail.com>
2019-01-30 20:55:20 -0600

    contact form try

--- client/src/components/ContactForm/index.js
+++ client/src/components/ContactForm/index.js
@@ -2,6 +2,11 @@ import React, {Component} from "react";
 import "./style.css";
 import { Form, FormGroup, Label, Input, Button } from "reactstrap";
 import API from '../../utils/API';
+import ReactGooglePlaceSuggest from "react-google-places-suggest";
+import ReactGoogleMapLoader from "react-google-maps-loader";
+import PropTypes from "prop-types";
+
+const API_KEY = "AIzaSyCaMSi6l2hlOVydP7rSq991gyuxNPSqGio";
 
 class ContactForm extends Component {
 
@@ -10,27 +15,30 @@ class ContactForm extends Component {
         phoneNumber: "",
         contactCategory: "Veterinarian",
         contactWebsite: "",
-        searchLocation: "",
-        suggestions: [],
-        hasLocation: false,
-        typedSearch: ""
+        // searchLocation: "",
+        // suggestions: [],
+        // hasLocation: false,
+        // typedSearch: ""
+        search: "",
+        value: "",
+        placeId: ""
     };
 
-    componentDidMount () {
+    // componentDidMount () {
 
-        this.initAutocomplete();
+    //     this.initAutocomplete();
 
-    }
+    // }
 
-    initAutocomplete = () => {
+    // initAutocomplete = () => {
       
         //this.autocomplete = new window.google.maps.places.AutocompleteService();
         //this.places = new window.google.maps.places.PlacesService(document.createElement('div'));
-        console.log("init");
+        // console.log("init");
 
-        console.log(this);
-        console.log(this.places);
-        console.log(this);
+        // console.log(this);
+        // console.log(this.places);
+        // console.log(this);
         // if (navigator.geolocation) {
         //     console.log(this);
             // navigator.geolocation.getCurrentPosition((position) => {
@@ -52,13 +60,13 @@ class ContactForm extends Component {
         // API.getLocation()
         //     .then(response => console.log(response));
 
-    };
+    // };
 
-    fetchPredictions = (value) =>
-    {
-        //search within about 5 miles of current location
-        console.log(this.geolocation);
-        console.log(value);
+    // fetchPredictions = (value) =>
+    // {
+    //     //search within about 5 miles of current location
+    //     console.log(this.geolocation);
+    //     console.log(value);
     
         // this.autocomplete.getPlacePredictions(
         //     {
@@ -68,53 +76,67 @@ class ContactForm extends Component {
         //         radius: 8000},
         //     this.autoCompletCallback
         // ); 
-    };
+    // };
 
-    autoCompletCallback = (predictions, status) =>
-    {
-        console.log(predictions);
+    // autoCompletCallback = (predictions, status) =>
+    // {
+    //     console.log(predictions);
 
-        this.setState({
-            suggestions: predictions.map((p,idx) => ({
-                id: p.place_id,
-                description: p.description,
-                index: idx
-            }))
-        });
-    }
+    //     this.setState({
+    //         suggestions: predictions.map((p,idx) => ({
+    //             id: p.place_id,
+    //             description: p.description,
+    //             index: idx
+    //         }))
+    //     });
+    // }
 
-    onChange = (e) => {
+    // onChange = (e) => {
         
-        this.fetchPredictions(e.target.value);
-    };
+        // this.fetchPredictions(e.target.value);
+    // };
 
-    getDetails = (index) => {
-        let selected_id = this.state.suggestions[index].id;
+    // getDetails = () => {
+    // //     let selected_id = this.state.suggestions[index].id;
+    //         let selected_id = this.state.placeId;
 
-        console.log(selected_id);
+    //         <ReactGoogleMapLoader
+    //             params={{
+    //                 key: API_KEY,
+    //                 libraries: "places, geocode",
+    //                 placeid: selected_id
+    //             }}
+    //             render={googleMaps =>
+    //                googleMaps && ( 
+    //                    console.log(res)
+    //                )}
+    //         />
+    // };
+            
+    //     console.log(selected_id);
         // this.places.getDetails(
         //     {
         //         placeId: selected_id,
         //         fields: ['name', 'website', 'formatted_phone_number']
         //     }, this.getDetailsCallback
         // );
-    };
+     //};
 
-    getDetailsCallback = (placeResults, status) => {
+    // getDetailsCallback = (placeResults, status) => {
 
-        console.log(placeResults);
-        console.log(status);
+    //     console.log(placeResults);
+    //     console.log(status);
 
-        this.setState({
-            contactName: placeResults.name,
-            contactWebsite: placeResults.website,
-            phoneNumber: placeResults.formatted_phone_number
-        });
+    //     this.setState({
+    //         contactName: placeResults.name,
+    //         contactWebsite: placeResults.website,
+    //         phoneNumber: placeResults.formatted_phone_number
+    //     });
 
-        this.setState({
-            suggestions: []
-        });
-    }
+    //     this.setState({
+    //         suggestions: []
+    //     });
+    // }
 
     handleSubmit = e => {
         // e.preventDefault()
@@ -130,44 +152,98 @@ class ContactForm extends Component {
         // }
     };
 
-    render() {
+    handleInputChange(e) {
+        this.setState({search: e.target.value, value: e.target.value});
+    }
 
+    handleSelectSuggest(suggest) {
+        console.log(suggest);
+        this.setState({search: "", placeId: suggest.place_id});
+        console.log(this.state.placeId);
+       // this.getDetails();
+    }
+
+    render() {
+        const {search, value} = this.state;
         return(
             <div>
-                <Form>
-                    <FormGroup>
-                        <Label for="contactCategory">Category</Label>
-                        <Input type="select" name="contactCategory" id="contactCategory">
-                            <option>Veterinarian</option>
-                            <option>Groomer</option>
-                            <option>Boarder</option>
-                            <option>Other</option>
-                        </Input>
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="contactName">Contact Name</Label>
-                        <Input type="text" name="contactName" id="contactName" onInput={this.onChange} />
-                        {
-                            this.state.suggestions.length > 0 ? 
-                            (<div >
-                                <ul className="suggestionsDiv">
-                                {this.state.suggestions.map(suggestion => 
-                                    {
-                                        return(
-                                            <li className="suggestions" onClick={() => this.getDetails(suggestion.index)}>{suggestion.description}</li>
-                                        )
-                                    })}
-                                </ul>
-                            </div>
-                            ) : <span className="clickedContact">{this.state.contactName}</span>
-
-                        }
-                    </FormGroup>
-                    <Button className="formBtn" style={{marginLeft: "52%"}} onClick={this.handleSubmit}>Submit</Button>
-                </Form>
+                <FormGroup>
+                    <Label for="contactCategory">Category</Label>
+                    <Input type="select" name="contactCategory" id="contactCategory">
+                        <option>Veterinarian</option>
+                        <option>Groomer</option>
+                        <option>Boarder</option>
+                        <option>Other</option>
+                    </Input>
+                     </FormGroup>
+            <ReactGoogleMapLoader
+                params={{
+                    key: API_KEY,
+                    libraries: "places, geocode",
+                }}
+                render={googleMaps =>
+                   googleMaps && (
+                    <div>
+                        <ReactGooglePlaceSuggest
+                        autocompletionRequest={{input: search}}
+                        googleMaps={googleMaps}
+                        onSelectSuggest={this.handleSelectSuggest.bind(this)}
+                        >
+                            <Label for="contactName">Contact Name</Label>
+                            <Input
+                                type="text"
+                                value={value}
+                                placeholder="Search For a Contact"
+                                onChange={this.handleInputChange.bind(this)}
+                                name="contactName"
+                                id="contactName"
+                                />
+                        </ReactGooglePlaceSuggest>
+                    </div>
+                    
+                )}
+                /> {/* end of maps-loader tag */}
+                <Button className="formBtn" style={{marginLeft: "52%", marginTop: "20px"}} onClick={this.handleSubmit}>Submit</Button>
             </div>
-        )
+        //     <div>
+        //         <Form>
+        //             <FormGroup>
+        //                 <Label for="contactCategory">Category</Label>
+        //                 <Input type="select" name="contactCategory" id="contactCategory">
+        //                     <option>Veterinarian</option>
+        //                     <option>Groomer</option>
+        //                     <option>Boarder</option>
+        //                     <option>Other</option>
+        //                 </Input>
+        //             </FormGroup>
+        //             <FormGroup>
+        //                 <Label for="contactName">Contact Name</Label>
+        //                 <Input type="text" name="contactName" id="contactName" onInput={this.onChange} />
+        //                 {
+        //                     this.state.suggestions.length > 0 ? 
+        //                     (<div >
+        //                         <ul className="suggestionsDiv">
+        //                         {this.state.suggestions.map(suggestion => 
+        //                             {
+        //                                 return(
+        //                                     <li className="suggestions" onClick={() => this.getDetails(suggestion.index)}>{suggestion.description}</li>
+        //                                 )
+        //                             })}
+        //                         </ul>
+        //                     </div>
+        //                     ) : <span className="clickedContact">{this.state.contactName}</span>
+
+        //                 }
+        //             </FormGroup>
+        //             <Button className="formBtn" style={{marginLeft: "52%"}} onClick={this.handleSubmit}>Submit</Button>
+        //         </Form>
+        //     </div>
+         )
     }
 }
 
-export default ContactForm
+ContactForm.propTypes = {
+    googleMaps: PropTypes.object,
+}
+
+export default ContactForm;
